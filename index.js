ba781c27642e84c0abbfda6fb5a5f9386f8db386
Sean Ye <seanye24@gmail.com>
2021-03-28 02:53:27 -0700

    Update bot cli

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
 const Bot = require('./Bot.js');
 require('dotenv').config();
 
-const monkey =
-    new Bot(process.env.MONKEY_USER, process.env.MONKEY_USER,
-            process.argv[2] || process.env.GAME_ID, process.env.MONKEY_TEAM);
+const bots = [
+  '[Bot] monkeybean',
+  '[Bot] gorillabean',
+  '[Bot] chimpbean',
+  '[Bot] baboonbean',
+];
 
-const gorilla =
-    new Bot(process.env.GORILLA_USER, process.env.GORILLA_USER,
-            process.argv[2] || process.env.GAME_ID, process.env.GORILLA_TEAM);
+if (argv.count === undefined) {
+  console.error('you must specify a bot count between 1-4 (eg --count 3)');
+  return;
+}
+if (argv.count <= 0 || argv.count > 4) {
+  console.error('bot count must be between 1-4');
+  return;
+}
 
-const chimp =
-    new Bot(process.env.CHIMP_USER, process.env.CHIMP_USER,
-            process.argv[2] || process.env.GAME_ID, process.env.CHIMP_TEAM);
-
-const baboon =
-    new Bot(process.env.BABOON_USER, process.env.BABOON_USER,
-            process.argv[2] || process.env.GAME_ID, process.env.BABOON_TEAM);
+bots.slice(0, argv.count).forEach((bot, i) => {
+  new Bot(bot, bot, argv.room || 'ffa', i + 1);
+});
